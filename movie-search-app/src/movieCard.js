d72488486efc5c8f3ca29e8b20837c1430aa4ddf
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-08-03 22:18:28 +0530

    update index.js

--- movie-search-app/src/movieCard.js
+++ movie-search-app/src/movieCard.js
@@ -6,7 +6,8 @@ export default function MovieCard(props){
       {props.movies.filter(movie => movie["poster_path"]).map(movie =>(
         <div className="card" key={movie["id"]}>
           <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie["poster_path"]}`} alt=""/>
-          <div className="car--component">
+
+          <div className="card--component">
             <h2 className="card--title">{movie["title"]}</h2>
             <p><small>RELEASE DATE: {movie["release_date"]}</small></p>
             <p><small>RATING: {movie["vote_average"]}</small></p>
