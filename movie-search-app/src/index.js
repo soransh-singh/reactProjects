2741a5794f8d2c7f1bc39c99f4fb4aeded2e5ade
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-08-03 22:43:48 +0530

    update

--- movie-search-app/src/index.js
+++ movie-search-app/src/index.js
@@ -10,9 +10,14 @@ import styles from './master.css'
 class Main extends React.Component {
   render(){
     return (
-      <div className="container">
-        <h1 className="title">Movie search App</h1>
-        <SearchMovies/>
+      <div>
+        <header>
+          <small>created by <a href="https://github.com/soransh-singh">soransh</a></small>
+        </header>
+        <main className="container">
+          <h1 className="title">Movie search App</h1>
+          <SearchMovies/>
+        </main>
       </div>
     );
   }
